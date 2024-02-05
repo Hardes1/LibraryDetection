0682d106c944efb3335efdabec4cf5e84222ef5e
GitHub Action <action@github.com>
2024-02-05 18:10:01 +0000

    Template cleanup

--- src/main/kotlin/com/github/hardes1/librarydetection/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/hardes1/librarydetection/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hardes1.librarydetection.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
